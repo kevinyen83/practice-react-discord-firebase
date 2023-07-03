004654ea20de6bd77242f5078f928e5af2e1f141
Kevin <kevinyenhaha@gmail.com>
2023-07-03 21:05:37 +1000

    v2

--- middleman_page/src/components/Home.js
+++ middleman_page/src/components/Home.js
@@ -7,26 +7,47 @@ import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import Modal from 'react-bootstrap/Modal';
 import callList from './callList';
 
 const Home = () => {
   const [data, setData] = useState(callList);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedTranscription, setSelectedTranscription] = useState("");
+  const [filter, setFilter] = useState(""); // State to store the filter value
+
+  const handleOpenModal = (transcription) => {
+    setSelectedTranscription(transcription);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedTranscription("");
+    setShowModal(false);
+  };
+
+  const handleFilter = (status) => {
+    setFilter(status);
+  };
+
+  const filteredData = filter ? data.filter((values) => values.status === filter) : data; // Apply filter to data
 
   const renderCards = () => {
-    return data.map((values) => {
-      const { id, firstName, lastName, addressLine, city, state, postCode, visitType, transcription } = values;
+    return filteredData.map((values) => {
+      const { id, firstName, lastName, addressLine, city, state, postCode, visitType, transcription, phone } = values;
+      const isSilent = visitType === "Silent"; // Check if Visit Type is "Silent"
+
       return (
         <Col key={id}>
           <CardGroup>
             <Card>
               <Card.Body>
                 <Card.Title>{firstName} {lastName}</Card.Title>
-                <Card.Text>
-                    <div className=''>{transcription}</div>
-                </Card.Text>
                 <Card.Text><b>Location: {addressLine} {city} {state} {postCode}</b></Card.Text>
-                <Card.Text><b>Visit Type: {visitType}</b></Card.Text>
-                <Button href='https://cozy-crepe-316d5e.netlify.app/' variant="primary">Answer the phone</Button>
+                <Card.Text><b>Phone: {phone}</b></Card.Text>
+                <Card.Text className={isSilent ? "text-danger" : ""}><b>Visit Type: {visitType}</b></Card.Text>
+                <Button className='mb-1' href='https://cozy-crepe-316d5e.netlify.app/' variant="primary">Answer the phone</Button>
+                <Button variant="secondary" onClick={() => handleOpenModal(transcription)}>Read transcription</Button>
               </Card.Body>
             </Card>
           </CardGroup>
@@ -38,21 +59,33 @@ const Home = () => {
   return (
     <Container fluid>
       <Row>
-        <Col md={2} className='bg_colour_a'>
+        <Col md={2} className='bg_colour_a mt-4'>
           <Row>
-            <ButtonGroup vertical>
-              <Button>Active calls</Button>
-              <Button>Pending calls</Button>
-              <Button>Missed calls</Button>
+            <ButtonGroup vertical >
+              <Button variant="light" onClick={() => handleFilter("Active")}>ACTIVE CALLS</Button>
+              <Button variant="light" onClick={() => handleFilter("Pending")}>PENDING CALLS</Button>
+              <Button variant="light" onClick={() => handleFilter("Missed")}>MISSED CALLS</Button>
             </ButtonGroup>
           </Row>
         </Col>
-        <Col className='bg_colour_b'>
-            <Row xs={1} md={4} className="g-4">
-                {renderCards()}
-            </Row>
+        <Col className='bg_colour_b mt-4'> {/* Add mt-4 class for margin-top spacing */}
+          <Row xs={1} md={4} className="g-4">
+            {renderCards()}
+          </Row>
         </Col>
       </Row>
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Transcription</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{selectedTranscription}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
--- middleman_page/src/components/NavbarPage.js
+++ middleman_page/src/components/NavbarPage.js
@@ -1,19 +1,27 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavbarPage() {
+  const handleReload = () => {
+    window.location.reload();
+  };
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar expand="xxl" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+      <Container fluid>
+        <Navbar.Brand onClick={handleReload}>DOLBY.IO</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link onClick={handleReload}>Home</Nav.Link>
           </Nav>
+          <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
         </Navbar.Collapse>
       </Container>
     </Navbar>
